624f84cc6290547624b3077dfa94d06485f91b9f
Rama Dwipa <ramadwipa>
2026-03-11 05:20:44 +0800

    install laravel to project

---
+++ resources/views/components/sidebar/sidebar-layout.blade.php
@@ -0,0 +1,20 @@
+<aside>
+    <div>
+        {{-- 
+        ==============
+        TITLE SIDEBAR
+        ==============
+    --}}
+        <div>
+            <h1>TICKET.COM</h1>
+        </div>
+        {{-- 
+        =============
+        MENU SIDEBAR
+        =============
+    --}}
+        <ul>
+            
+        </ul>
+    </div>
+</aside>
