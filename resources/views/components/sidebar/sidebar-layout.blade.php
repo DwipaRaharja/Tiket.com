3eed543730b16e0b2350067f45641f4311ab70a8
Rama Dwipa <ramadwipa>
2026-03-11 08:34:31 +0800

    Menambahkan sidebar ke admin

--- resources/views/components/sidebar/sidebar-layout.blade.php
+++ resources/views/components/sidebar/sidebar-layout.blade.php
@@ -1,20 +1,57 @@
-<aside>
-    <div>
+<aside class="min-h-screen max-w-56 bg-mist-800">
+    <div class="flex flex-col gap-3">
         {{-- 
         ==============
         TITLE SIDEBAR
         ==============
     --}}
-        <div>
-            <h1>TICKET.COM</h1>
+        <div class="py-2">
+            <h1 class="text-center text-2xl font-bold text-white">TICKET.COM</h1>
         </div>
         {{-- 
-        =============
-        MENU SIDEBAR
-        =============
-    --}}
-        <ul>
-            
+        ==================
+        LIST MENU SIDEBAR
+        ==================
+        --}}
+        <ul class="flex flex-col gap-2">
+            <x-sidebar.sidebar-link name="dashboard">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
+                />
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
+                />
+            </x-sidebar.sidebar-link>
+            <x-sidebar.sidebar-link name="manage bus">
+                <path d="M4 6 2 7" />
+                <path d="M10 6h4" />
+                <path d="m22 7-2-1" />
+                <rect width="16" height="16" x="4" y="3" rx="2" />
+                <path d="M4 11h16" />
+                <path d="M8 15h.01" />
+                <path d="M16 15h.01" />
+                <path d="M6 19v2" />
+                <path d="M18 21v-2" />
+                >
+            </x-sidebar.sidebar-link>
+            <x-sidebar.sidebar-link name="manage customers">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+            </x-sidebar.sidebar-link>
+            <x-sidebar.sidebar-link name="manage schedules">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+                />
+            </x-sidebar.sidebar-link>
         </ul>
     </div>
 </aside>
